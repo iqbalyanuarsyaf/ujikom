6d0889c9f046b403149fdc78f7ca1cd62336f5a8
iqbalyanuarsyaf <141013074+iqbalyanuarsyaf@users.noreply.github.com>
2024-02-21 14:42:29 +0700

    Add files via upload

--- UKK_iqbalyanuarsyaf/admin/index.php
+++ UKK_iqbalyanuarsyaf/admin/index.php
@@ -28,6 +28,7 @@ if ($_SESSION['status'] != 'login') {
     <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
       <div class="navbar-nav me-auto">
        <a href="album.php" class="nav-link">Album</a>
+       <a href="foto.php" class="nav-link">Foto</a>
       </div>
       
       <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
